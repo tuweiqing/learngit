e09d84fea1f36f7f1e918309fdbe0faef49eed7d
tuweiqing <Weiqing@bogon>
2017-07-29 06:53:48 +0800

    Initial Commit

---
+++ LearnGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnGit
+//
+//  Created by 涂伟青 on 2017/7/29.
+//  Copyright © 2017年 Viking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
